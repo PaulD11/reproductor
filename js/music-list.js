eec020527ba23b6d41080bb86acb6d268b42e04a
PaulD11 <106404323+PaulD11@users.noreply.github.com>
2022-06-15 19:35:56 -0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+/*Crearemos un array(array:es una variable estructurada formada de un número "n" de variables 
+simples del mismo tipo que son denominadas los componentes o elementos del array) 
+y dentro de este array crearemos los objetos en donde incluiremos el nombre de la cancion, artista, etc.*/
+
+/*Let nos permite declarar variables limitando su alcance. Let puede ser modificado pero no re-declaradas*/
+
+let allMusic = [
+  {
+    name: "Unforgettable",
+    artist: "French Montana",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "After Dark",
+    artist: "Mr.Kitty",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dealer",
+    artist: "Lana Del Rey ft. Miles Kane",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lovers Rock",
+    artist: "Tv Girl",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Cigarette Daydreams",
+    artist: "Cage The Elephant",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "See you again",
+    artist: "Tyler the Creator ft. Kali Uchis",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
